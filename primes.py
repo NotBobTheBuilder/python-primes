1c5af2a2fbd8c651dd46609387f40afaf5fe0871
Jack Wearden <jack@jackwearden.co.uk>
2014-04-11 23:17:14 +0100

    Primes

---
+++ primes.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+from itertools import count
+
+class Primes(object):
+    cached = [2]
+    def __init__(self, stop=None):
+        self.stop = stop or float('inf')
+
+    def __iter__(self):
+        yield 1
+        for prime in Primes.cached:
+            yield prime
+
+        start = Primes.cached[-1]
+        for num in count(start + 1, 2):
+            if num > self.stop:
+                break
+
+            if not any(num % p == 0 for p in Primes.cached):
+                Primes.cached.append(num)
+                yield num
